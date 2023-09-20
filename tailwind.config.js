7d5c96a6e87fa6ec641a7b29f3e88ef3782d2b4d
w3zeblamf <zeblamfw3@gmail.com>
2023-09-19 20:33:59 -0400

    new info added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ghNav': '#010409',
+        'ghBody': '#0D1117'
+      }
+    },
   },
   plugins: [],
 }
